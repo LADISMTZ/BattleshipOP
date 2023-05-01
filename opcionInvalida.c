b4dbb2bc2c574aaf8459634309ce2b02f6bdacd8
LADISMTZ <122698443+LADISMTZ@users.noreply.github.com>
2023-05-01 14:43:44 -0600

    Add files via upload

---
+++ opcionInvalida.c
@@ -0,0 +1,11 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	printf("\nLos parametros que seleccionaste sobrepasan el tama'o de la matriz\n");
+	printf("Inicia de nuevo\n");
+
+    return 0;
+}
